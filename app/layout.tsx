84d1a48e60772e48c804283930d94d36bf2a3064
AyyanTube <shahanaakhter1979@icloud.com>
2024-04-04 18:57:50 +0600

    oki
    i

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "SafwanFolio",
+  description: "I am a soon to be full stack front end developer.",
 };
 
 export default function RootLayout({
